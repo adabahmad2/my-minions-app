e3aae963503ba6686733ed26c7f7bf0ef44d872f
adabahmad2 <adabahmad2@gmail.com>
2022-06-08 03:55:36 +0530

    imported css

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var name=prompt("What is your name?");
-alert("this script works!" + name); 
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+function clickEventHandler()
+{
+    console.log("clicked");
+    console.log("Userinput = ", txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickEventHandler)
