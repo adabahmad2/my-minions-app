ac13114587e0be532bec07d2fc543f2bf3d387e3
adabahmad2 <adabahmad2@gmail.com>
2022-06-17 04:07:51 +0530

    mock server implemented

--- app.js
+++ app.js
@@ -2,9 +2,35 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#output");
 
+var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(userInput)
+{
+    return serverUrl + "?" + "text=" + userInput;
+}
+
+function errorHandler(error)
+{
+    console.log("error occured", error);
+    alert("The Minion is full or your food is stale, Kindly try again in sometime :/")
+}
+
+
 function clickEventHandler()
 {
-    txtOutput.innerText = "hjfhdjfhdfu : " + txtInput.value;
-}
+    //taking input to send to server
+    var InputText = txtInput.value;
+
+    //calling the server to process the input
+    fetch(getTranslationURL(InputText))
+    .then(response => response.json())
+    .then(json => {
+        //output the translated text
+        txtOutput.innerText = json.contents.translated;
+        console.log(json.contents.translated)
+    })
+    .catch(errorHandler);
+};
+
 
 btnTranslate.addEventListener("click", clickEventHandler)
